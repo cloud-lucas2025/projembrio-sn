89a06522238c5ef81160a8ab45b545e3ee309f00
cloud-lucas2025 <cloudlua1@gmail.com>
2025-07-02 16:00:33 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Embriologia Simplificada - SN',
+  title: 'Embriologia Simplificada - DN',
   description: 'Criado para Fins Educacionais',
   generator: '.dev',
 }
